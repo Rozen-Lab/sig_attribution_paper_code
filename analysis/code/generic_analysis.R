27542f6d94b3b9975065bfc1f28f4acdc53c58a3
Steve Rozen <steverozen+github@pm.me>
2024-09-04 06:00:04 +0800

    generic code for testing

---
+++ analysis/code/generic_analysis.R
@@ -0,0 +1,119 @@
+source("analysis/code/analysis_utils.R")
+
+run_generic_msi <-
+  function(spectra, sig, cancer_type, more_args, attribute_function) {
+    all_retvals <- list()
+    msi_sig_names <- get_msi_sig_names()
+    
+    # Check whether there are any MSI-H samples in spectra
+    msi_samples <- grep(pattern = "MSI", x = colnames(spectra), value = TRUE)
+
+    if (length(msi_samples) > 0) {      
+      spectra_msi <- spectra[, msi_samples, drop = FALSE]
+
+      non_msi_samples <- setdiff(colnames(spectra), msi_samples)
+      spectra_non_msi <- spectra[, non_msi_samples, drop = FALSE]
+      non_msi_sig_names <- setdiff(colnames(sig), msi_sig_names)
+      
+      retval_msi <-
+        attribute_function(
+          spectra = spectra_msi,
+          signatures = sig, # All signatures, including MSI signatures
+          more_args
+        )
+      
+      all_retvals[[paste0(cancer_type, "_msi")]] <- retval_msi
+      retval_key <- paste0(cancer_type, "_non_msi")
+      
+    } else {
+      non_msi_sig_names <- colnames(sig)
+      non_msi_samples <- colnames(spectra)
+      retval_key <- cancer_type
+    }
+    
+    retval_non_msi <-
+      attribute_function(
+        spectra = spectra[ , non_msi_samples, drop = FALSE],
+        signatures = sig[, non_msi_sig_names, drop = FALSE],
+        more_args
+      )
+    
+    all_retvals[[retval_key]] <- retval_non_msi
+
+    return(all_retvals)
+  }
+
+
+
+run_generic_syn <- function(dataset_name, output_home,
+                                data_top_folder_name = "synthetic_data",
+                                cancer_types = NULL,
+                                attribute_function,
+                                more_args) {
+  message("Start running the job")
+  
+  if (!dir.exists(output_home)) {
+    dir.create(path = output_home, recursive = TRUE)
+  }
+  
+  all_inputs <- get_all_input(
+    dataset_name = dataset_name,
+    data_top_folder_name = data_top_folder_name
+  )
+  all_spectra <- all_inputs$spectra_list
+  gt_sig <- all_inputs$ground_truth_sigs
+  sig_universe <- all_inputs$signature_universes
+  
+  if (is.null(cancer_types)) {
+    cancer_types <- all_inputs$cancer_types
+  }
+  
+  all_retvals <- list()
+  time_by_cancer_type <- list()
+  for (cancer_type in cancer_types) {
+    spectra <- all_spectra[[cancer_type]]
+    sig_names <- names(sig_universe[[cancer_type]])
+    sig <- gt_sig[, sig_names, drop = FALSE]
+    output_dir <- file.path(output_home, cancer_type)
+    
+    time_used <- system.time({
+      
+      retval <-
+        run_generic_msi(
+          spectra = spectra,
+          sig = sig,
+          cancer_type = cancer_type,
+          attribute_function = attribute_function,
+          more_args
+        )
+    }) # system.time
+    
+    time_by_cancer_type[[cancer_type]] <- time_used
+    all_retvals <- c(all_retvals, retval)
+    
+  } # for (cancer_type in cancer_types...
+  
+  saveRDS(time_by_cancer_type,
+          file.path(output_home, "time_by_cancer_type.Rds"))
+  
+  final_exposure <- mSigAct:::MergeListOfExposures(all_retvals)
+  
+  original_spectra_file <-
+    file.path(data_top_folder_name, dataset_name, "ground.truth.syn.catalog.csv")
+  
+  original_spectra <- ICAMS::ReadCatalog(original_spectra_file)
+  
+  if (setequal(colnames(original_spectra), colnames(final_exposure))) {
+    final_exposure <-
+      final_exposure[, colnames(original_spectra), drop = FALSE]
+  }
+  
+  mSigTools::write_exposure(
+    exposure = final_exposure,
+    file = file.path(output_home, "inferred_exposures.csv")
+  )
+  
+  saveRDS(all_retvals, file = file.path(output_home, "all_retvals.Rds"))
+  message("Finished running all the jobs")
+}
+
