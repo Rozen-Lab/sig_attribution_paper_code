243d7059fa7a1c26ed5f805f3d661186b6ad6b03
Steve Rozen <steverozen+github@pm.me>
2024-09-04 02:18:55 +0800

    wrapped everything in function to avoid confusion over globals

--- analysis/code/SBS/01_run_pasa_syn_SBS.R
+++ analysis/code/SBS/01_run_pasa_syn_SBS.R
@@ -1,19 +1,25 @@
 # Run this script with the top level directory as the working directory
-source("analysis/code/analysis_utils.R")
+source("analysis/code/pasa_analysis.R")
 
-output_home <- "analysis/raw_output/SBS/pasa/syn"
-total_cores <- parallel::detectCores()
-mc_cores_per_sample <- min(5, total_cores)
-num_parallel_samples <- floor(total_cores / mc_cores_per_sample)
 
-time_used <- system.time({
-  run_pasa_syn(
-    dataset_name = "SBS",
-    output_home = output_home,
-    seed_in_use = 145879,
-    num_parallel_samples = num_parallel_samples,
-    mc_cores_per_sample = mc_cores_per_sample
-  )
-})
+run_pasa = function() {
+  output_home <- "analysis/raw_output/SBS/pasa/syn"
+  total_cores <- parallel::detectCores()
+  mc_cores_per_sample <- min(5, total_cores)
+  num_parallel_samples <- floor(total_cores / mc_cores_per_sample)
+  
+  time_used <- system.time({
+    run_pasa_syn(
+      dataset_name = "SBS",
+      output_home = output_home,
+      seed_in_use = 145879,
+      num_parallel_samples = num_parallel_samples,
+      mc_cores_per_sample = mc_cores_per_sample
+    )
+  })
+  
+  saveRDS(time_used, file = file.path(output_home, "time_used.Rds"))
+}
 
-saveRDS(time_used, file = file.path(output_home, "time_used.Rds"))
+run_pasa()
+
