8d270fd7804dfdb5b3effd95b36e98465b64dd9c
Steve Rozen <steverozen+github@pm.me>
2024-09-05 02:16:50 +0800

    more code refactoring

--- analysis/code/deconstruct_analysis.R
+++ analysis/code/deconstruct_analysis.R
@@ -1,12 +1,14 @@
 # remotes::install_github("raerose01/deconstructSigs") # Version 1.9.0
 library(deconstructSigs)
 
+source("analysis/code/generic_analysis.R")
+
 call_deconstruct <- function(spectra, 
                              signatures, 
                              more_args
                              ) {
   spectra <- t(spectra)
-  spectra <- spectra / rowSums(spectra) # This software runs on proporitons
+  spectra <- spectra / rowSums(spectra) # This software runs on proportions
   spectra <- as.data.frame(spectra)
   signatures <-as.data.frame(t(signatures))
 
@@ -28,5 +30,24 @@ call_deconstruct <- function(spectra,
   # and rows signatures
   weights <- lapply(allretval, `[[`, "weights")
   exposure_matrix <- t(do.call(rbind, weights))
+  browser()
   return(exposure_matrix)
+}
+
+run_deconstruct <- function(mut_type, cutoff) {
+  out_fragment = paste0("deconstruct_", formatC(cutoff, digits = 2, format = "f"))
+  more_args = list(signature_cutoff = cutoff) 
+  output_home <-
+    file.path("analysis/raw_output", mut_type, out_fragment, "syn")
+  message("deconstructSigs, writing to ", output_home)
+  time_used <- system.time({
+    run_generic_syn(
+      dataset_name = mut_type,
+      output_home = output_home,
+      attribute_function = call_deconstruct,
+      more_args = more_args
+    )
+  })
+  
+  saveRDS(time_used, file = file.path(output_home, "time_used.Rds"))
 }
