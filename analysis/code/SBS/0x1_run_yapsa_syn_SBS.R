3cf33afe65215b8ac95a67f813ee828dfbeaebb7
Steve Rozen <steverozen+github@pm.me>
2024-09-05 02:25:16 +0800

    more code refactoring

---
+++ analysis/code/SBS/0x1_run_yapsa_syn_SBS.R
@@ -0,0 +1,9 @@
+# Run this script with the top level directory as the working directory
+stopifnot(basename(getwd()) == "sig_attribution_paper_code")
+
+source("analysis/code/yapsa_analysis.R")
+
+# default cutoff is 0
+for (cutoff in c(0, 0.01, 0.03, 0.06, 0.1)) {
+  run_yapsa("SBS", cutoff)
+}
