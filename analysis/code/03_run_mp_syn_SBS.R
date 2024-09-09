d4dc9ef4cabbcdc474ff69df18d4d5a3c7cb0b05
Steve Rozen <steverozen+github@pm.me>
2024-09-10 04:15:10 +0800

    file in wrong place ; a dup

--- analysis/code/03_run_mp_syn_SBS.R
+++
@@ -1,6 +0,0 @@
-# Run this script with the top level directory as the working directory
-stopifnot(basename(getwd()) == "sig_attribution_paper_code")
-
-source("analysis/code/mp_analysis.R")
-
-run_mp("SBS")
