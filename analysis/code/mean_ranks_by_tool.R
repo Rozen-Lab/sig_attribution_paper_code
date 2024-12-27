be1592040377a81d0ea1df10d59d8ed0ce6cf532
Steven G. Rozen <steve@steverozen.net>
2024-12-27 14:39:26 -0500

    More summary statistics on ranks across cancer types

--- analysis/code/mean_ranks_by_tool.R
+++ analysis/code/mean_ranks_by_tool.R
@@ -2,61 +2,75 @@ library(ggplot2)
 library(ggbeeswarm)
 library(gridExtra)
 library(dplyr)
+library(data.table)
+library(huxtable)
 
-do_plot_ranks_by_tool = FALSE
-
-mean_ranks_by_tool = function(mutation_type) {
+ranks_by_tool = function(mutation_type, mean_best_worst) {
   ranktable = fread(
     paste0("output_for_paper/tools_ranked_in_each_cancer_type_",
     mutation_type,
     ".csv"))
 
-  
   ranktable = group_by(ranktable, Tool)
   
+  if (mean_best_worst == "mean") {
+    myfn = mean
+    newcolname = "Mean rank"
+    partfilename = "_mean"
+  } else if (mean_best_worst == "best") {
+    myfn = min
+    newcolname = "Best rank"
+    partfilename = "_best"
+  } else if (mean_best_worst == "worst") {
+    myfn = max
+    newcolname = "Worst rank"
+    partfilename = "_worst"
+  } else if (mean_best_worst == "sd") {
+    myfn = sd
+    newcolname = "Standard deviation of rank"
+    partfilename = "_sd"
+  } else {
+    stop("unrecognized measure: ", mean_best_worst)
+  }
+
   means <- ranktable %>%
     group_by(Tool) %>%
-    summarize(mean_rank = mean(`Rank within cancer type`)) %>%
+    summarize(target = myfn(`Rank within cancer type`)) %>%
     ungroup() %>%
-    arrange(mean_rank)
+    arrange(target)
+  
   
   means %>%
-    mutate(`Cancer type` = "", rank = "") %>%
-    relocate(`Cancer type`, Tool, rank, mean_rank) %>%
-    rename(`Mean rank` = mean_rank) %>%
-    huxtable::as_huxtable() %>%
-    huxtable::set_align("centre") %>%
-    huxtable::set_number_format(NA) %>%
-    quick_xlsx(file = 
-                 file.path("output_for_paper",
-                           paste0("add_this_manually_to_sup_table_for_",
-                                  mutation_type, "_rank.xlsx")))
+    mutate(`Cancer type` = "", rank = NULL) %>%
+    relocate(`Cancer type`, Tool, target) ->
+    table2
+
+    colnames(table2) = c("", "Tool", newcolname)
+
+    table2 %>%
+      huxtable::as_huxtable() %>%
+      huxtable::set_align("centre") %>%
+      huxtable::set_number_format(NA) %>%
+      huxtable::insert_row(
+        paste("Summary,", mean_best_worst, "ranks for",
+              mutation_type, "signatures"),
+        fill = "") %>%
+      huxtable::merge_across(1, 1:3) %>%
+      huxtable::set_header_rows(1:2, TRUE) %>%
+      huxtable::style_header_rows(bold = TRUE) %>%
+      quick_xlsx(
+        file = 
+          file.path("output_for_paper",
+                    paste0("add_this_manually_to_sup_table_for_",
+                           mutation_type,
+                           partfilename,
+                           "_rank.xlsx")))
   
-  if (do_plot_ranks_by_tool) {
-    
-    ggplot(ranktable, aes(x = Tool, y = `Rank within cancer type`, group = Tool)) +
-      geom_beeswarm(aes(color = `Cancer type`), size = 2) +
-      theme_minimal() +
-      labs(
-        title = paste("Ranks for", mutation_type, "within each cancer type"),
-        x = "Tool",
-        y = "Rank",
-        color = "Cancer Type"
-      ) +
-      theme(
-        axis.text.x = element_text(angle = 45, hjust = 1),
-        panel.grid.major.y = element_line(color = "grey90", size = 0.5)
-      ) +
-      scale_y_reverse(breaks = 1:13)
+}
+
+for (mutation_type in c("SBS", "DBS", "ID")) {
+  for (rank_type in c("mean", "best", "worst", "sd")) {
+    ranks_by_tool(mutation_type, rank_type)
   }
 }
 
-if (do_plot_ranks_by_tool ) {
-  # This plot is not used in the paper
-  grid.arrange(
-    plot_ranks_by_tool("SBS"),
-    plot_ranks_by_tool("DBS"),
-    plot_ranks_by_tool("ID"),
-    ncol = 1, 
-    nrow = 3  )
-}
